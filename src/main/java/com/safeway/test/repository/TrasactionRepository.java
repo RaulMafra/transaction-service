5ac23a6ec7b93e60fca4cc5f838b19e8d5c1024b
RaulMafra <raulcesar.sm@gmail.com>
2024-01-21 02:57:20 -0300

    refactor: fixed class name

--- src/main/java/com/safeway/test/repository/TrasactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.safeway.test.repository;
-
-import com.safeway.test.domain.transaction.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrasactionRepository extends JpaRepository<Transaction, Long> {
-}
