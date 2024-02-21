de04abaa97472835e38a830b9d71405393fe71a2
RaulMafra <raulcesar.sm@gmail.com>
2024-02-21 14:55:41 -0300

    feat: created exception for fields null

---
+++ src/main/java/com/transaction/service/exception/exceptions/IllegalFieldException.java
@@ -0,0 +1,8 @@
+package com.transaction.service.exception.exceptions;
+
+public class IllegalFieldException extends RestException {
+
+    public IllegalFieldException(String msg){
+        super(msg);
+    }
+}
