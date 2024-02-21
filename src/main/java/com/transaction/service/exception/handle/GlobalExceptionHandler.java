6becb08e17d1b40a28647202cc7112b321b4d43b
RaulMafra <raulcesar.sm@gmail.com>
2024-02-21 14:56:23 -0300

    feat: added exception for fields null

--- src/main/java/com/transaction/service/exception/handle/GlobalExceptionHandler.java
+++ src/main/java/com/transaction/service/exception/handle/GlobalExceptionHandler.java
@@ -1,8 +1,9 @@
 package com.transaction.service.exception.handle;
 
 import com.amazonaws.AmazonClientException;
-import com.transaction.service.exception.exceptions.IllegalFormattingException;
+import com.transaction.service.exception.exceptions.IllegalFieldException;
 import com.transaction.service.exception.exceptions.RestException;
+import com.transaction.service.exception.exceptions.UserNotFound;
 import com.transaction.service.exception.exceptions.WebhookException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -19,7 +20,7 @@ import java.net.UnknownHostException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    private org.springframework.http.HttpHeaders headers() {
+    private HttpHeaders headers() {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         return headers;
@@ -28,35 +29,40 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({WebhookException.class, UnknownHostException.class})
     private ResponseEntity<ResponseError> handleWebhookException(WebhookException e, WebRequest request, HttpServletRequest servletRequest) {
-        String response = "Servico de envio de confirmacao da transacao indisponivel";
-        ResponseError responseError = new ResponseError(HttpStatus.SERVICE_UNAVAILABLE.value(), HttpStatus.SERVICE_UNAVAILABLE, response, servletRequest.getRequestURI());
+        ResponseError responseError = new ResponseError(HttpStatus.SERVICE_UNAVAILABLE.value(), HttpStatus.SERVICE_UNAVAILABLE, e.getMessage(), servletRequest.getRequestURI());
         return new ResponseEntity<>(responseError, headers(), HttpStatus.SERVICE_UNAVAILABLE);
     }
 
     @ExceptionHandler(AmazonClientException.class)
     private ResponseEntity<ResponseError> handleWebServiceException(AmazonClientException e, WebRequest request, HttpServletRequest servletRequest) {
-        String response = "Servico de e-mail indisponivel";
+        String response = "Email service unavailable";
         ResponseError responseError = new ResponseError(HttpStatus.SERVICE_UNAVAILABLE.value(), HttpStatus.SERVICE_UNAVAILABLE, response, servletRequest.getRequestURI());
         return new ResponseEntity<>(responseError, headers(), HttpStatus.SERVICE_UNAVAILABLE);
     }
 
-    @ExceptionHandler(IllegalFormattingException.class)
-    private ResponseEntity<ResponseError> handleFormattingExceptions(IllegalFormattingException e, WebRequest request, HttpServletRequest servletRequest){
+    @ExceptionHandler(IllegalFieldException.class)
+    private ResponseEntity<ResponseError> handleFieldExceptions(IllegalFieldException e, WebRequest request, HttpServletRequest servletRequest){
         ResponseError responseError = new ResponseError(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, e.getMessage(), servletRequest.getRequestURI());
         return new ResponseEntity<>(responseError, headers(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(RestException.class)
     private ResponseEntity<ResponseError> handleRestApiException(RestException e, WebRequest request, HttpServletRequest servletRequest){
+        ResponseError responseError = new ResponseError(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, e.getMessage(), servletRequest.getRequestURI());
+        return new ResponseEntity<>(responseError, headers(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserNotFound.class)
+     private ResponseEntity<ResponseError> handleUserNotFound(UserNotFound e, WebRequest request, HttpServletRequest servletRequest){
         ResponseError responseError = new ResponseError(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND, e.getMessage(), servletRequest.getRequestURI());
         return new ResponseEntity<>(responseError, headers(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     private ResponseEntity<ResponseError> handleDataPersistence(DataIntegrityViolationException e, WebRequest request, HttpServletRequest servletRequest){
-        String response = "O documento ou o email ja esta em uso";
-        ResponseError responseError = new ResponseError(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, response, servletRequest.getRequestURI());
-        return new ResponseEntity<>(responseError, headers(), HttpStatus.BAD_REQUEST);
+        String response = "Conflict when persisting the client";
+        ResponseError responseError = new ResponseError(HttpStatus.CONFLICT.value(), HttpStatus.CONFLICT, response, servletRequest.getRequestURI());
+        return new ResponseEntity<>(responseError, headers(), HttpStatus.CONFLICT);
     }
 
 }
