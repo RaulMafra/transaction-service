59a885a327ddea62c28e5721b1be21d33cef52a8
RaulMafra <raulcesar.sm@gmail.com>
2024-02-21 14:57:22 -0300

    feat: added overload of the constructor

--- src/main/java/com/transaction/service/exception/exceptions/RestException.java
+++ src/main/java/com/transaction/service/exception/exceptions/RestException.java
@@ -5,4 +5,8 @@ public class RestException extends RuntimeException{
     public RestException(String msg){
         super(msg);
     }
+
+    public RestException(String msg, Throwable cause){
+        super(msg, cause);
+    }
 }
