1f1ecf631fadfe58639fefc5330be04da5ff1726
RaulMafra <raulcesar.sm@gmail.com>
2024-03-06 14:52:23 -0300

    test: test refactored

--- src/test/java/com/transaction/service/callback/service/WebhookServiceTest.java
+++ src/test/java/com/transaction/service/callback/service/WebhookServiceTest.java
@@ -4,38 +4,23 @@ import com.transaction.service.domain.transaction.Transaction;
 import com.transaction.service.domain.transaction.TransactionType;
 import com.transaction.service.domain.user.Client;
 import com.transaction.service.domain.user.Company;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import net.bytebuddy.description.type.TypeList;
-import org.hibernate.annotations.AnyDiscriminatorValue;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
-import org.springframework.stereotype.Service;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.net.MalformedURLException;
-import java.net.SocketTimeoutException;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 
@@ -48,13 +33,13 @@ class WebhookServiceTest {
     @InjectMocks
     private WebhookService webhookService;
 
-    String url;
+    String uri;
 
     Transaction transaction;
 
     @BeforeEach
     void init(){
-        url = "https://webhook.site/d62491e0-edaa-4608-ab3b-76e31e3a1b39";
+        uri = "https://webhook.site/d62491e0-edaa-4608-ab3b-76e31e3a1b39";
 
         Client client = new Client(UUID.randomUUID(), "example", "11111111100",
                 "example@teste.com", new BigDecimal("200"));
@@ -69,10 +54,12 @@ class WebhookServiceTest {
     void should_send_a_callback_with_successfully() {
         HttpEntity<Transaction> request = new HttpEntity<>(transaction);
 
-        webhookService.setUrl(url);
+        webhookService.setUrl(uri);
         assertDoesNotThrow(() -> this.webhookService.sendInfoTransaction(transaction));
+
         verify(restTemplate, times(1)).exchange("https://webhook.site/d62491e0-edaa-4608-ab3b-76e31e3a1b39", HttpMethod.POST,
                 request, String.class);
+        verifyNoMoreInteractions(restTemplate);
 
     }
 
@@ -82,6 +69,7 @@ class WebhookServiceTest {
         HttpEntity<Transaction> request = new HttpEntity<>(transaction);
 
         webhookService.setUrl(null);
+
         assertThrows(RuntimeException.class, () -> this.webhookService.sendInfoTransaction(transaction));
         verifyNoInteractions(restTemplate);
 
