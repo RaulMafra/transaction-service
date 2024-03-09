34b59689365f0f508156be2f91f7378a95f0de4e
RaulMafra <raulcesar.sm@gmail.com>
2024-03-09 15:59:05 -0300

    refactor: removed findCompanyById method

--- src/main/java/com/transaction/service/repository/CompanyRepository.java
+++ src/main/java/com/transaction/service/repository/CompanyRepository.java
@@ -4,10 +4,8 @@ import com.transaction.service.domain.user.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
-    Optional<Company> findCompanyById(Long id);
     Optional<Company> findCompanyByDocument(String document);
 }
